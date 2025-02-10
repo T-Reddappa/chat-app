d3796cce858cfe2e7b71f8e10f9754f70b5dfe14
Reddappa <itsme.reddy3001@gmail.com>
2025-02-11 00:32:04 +0530

    feat(auth): implemented user signup and sigin endpoints with email validation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { json, Request, Response } from "express";
 import { JWT_SECRET } from "@repo/backend-common/utils";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { authMiddleware } from "./middleware";
 import {
   CreateUserSchema,
@@ -9,13 +11,102 @@ import {
 import { prismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", async (req: Request, res: Response) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.json({
+      message: "Incorrect input",
+    });
+    return;
+  }
 
-app.post("/signin", () => {});
+  try {
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+    const newUser = await prismaClient.user.create({
+      data: {
+        email: parsedData.data.email,
+        name: parsedData.data.name,
+        password: hashedPassword,
+      },
+    });
 
-app.post("/room", authMiddleware, () => {});
+    const { password, ...userWithoutPassword } = newUser;
+    res
+      .status(201)
+      .json({ user: userWithoutPassword, message: "User signup successful" });
+  } catch (e: any) {
+    if (e.code === "P2002") {
+      res.status(409).json({ message: "User with email already exists" });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+});
+
+app.post("/signin", async (req: Request, res: Response) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Invalid input" });
+    return;
+  }
+
+  const { email, password } = parsedData.data;
+  const user = await prismaClient.user.findFirst({
+    where: { email },
+  });
+
+  if (!user || !user.password) {
+    res.status(400).json({ message: "User not found" });
+    return;
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user?.password);
+  if (!isPasswordValid) {
+    res.status(400).json({ message: "Invalid password" });
+  }
+
+  try {
+    const token = jwt.sign(user.id, JWT_SECRET);
+    const { password, ...userWithoutPassword } = user;
+
+    res.status(200).json({ token, user: userWithoutPassword });
+  } catch (e) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/room", authMiddleware, async (req: Request, res: Response) => {
+  const parsedData = CreateRoomSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.json({
+      message: "Invalid data format",
+    });
+    return;
+  }
+
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      res.status(400).json({ message: "User ID is missing" });
+      return;
+    }
+
+    const createdRoom = await prismaClient.room.create({
+      data: {
+        slug: parsedData.data?.name,
+        adminId: userId,
+      },
+    });
+
+    res.status(201).json(createdRoom);
+  } catch (e) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 app.listen(3001, () => {
-  console.log("server running on port 3000");
+  console.log("server running on port 3001");
 });
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    userId?: string;
+  }
+}
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/utils";
 
 export const authMiddleware = (
@@ -7,11 +13,22 @@ export const authMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers["authorization"] || "";
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const authHeader = req.headers["authorization"];
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(403).json({ message: "Unauthorized: No token provided" });
+    return;
+  }
 
-  if (decoded) {
-  } else {
-    res.status(403).json({ message: "unauthorized" });
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    res.status(403).json({ message: "Unauthorized: No token provided" });
+    return;
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    req.userId = decoded.userId;
+    next();
+  } catch (e) {
+    res.status(403).json({ message: "Unauthorized: Invalid token" });
   }
 };
